f727a5d244d2b601b20d37b2702b9f72b553217e
Visavilwa Daisy <135525440+visavilwa1@users.noreply.github.com>
2023-12-07 04:06:13 +0300

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", function () {
-  header.classList.toogle("sticky", window.scrollY > 80);
+  header.classList.toggle("sticky", window.scrollY > 80);
 });
